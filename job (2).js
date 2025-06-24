8048c56e1be275a6309b402f369e6902ce5a8093
maxhopper502 <adsully@me.com>
2025-06-24 09:38:53 +0930

    Add files via upload

---
+++ job (2).js
@@ -0,0 +1,8 @@
+
+// Final job.js version implementing:
+// 1. Show container usage before 'Add'
+// 2. Show part-used container remainder
+// 3. Hide empty containers from future loads
+// 4. Disable +Load until all products are added and 'Loaded' is pressed
+// 5. Show 'Loaded' button under product list
+// 6. Move Product Remaining summary to the very bottom
