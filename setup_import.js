240dfb82ad230fba1bb67475ab925bf77a103790
maxhopper502 <adsully@me.com>
2025-06-27 16:21:16 +0930

    Delete setup_import.js

--- setup_import.js
+++
@@ -1,9 +0,0 @@
-
-// This script handles importing jobs with multiple container sizes
-window.onload = () => {
-  const importBtn = document.getElementById("importFile");
-  importBtn.addEventListener("change", () => {
-    // mock parse logic
-    alert("📥 Importing file...");
-  });
-};
